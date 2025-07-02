0f8016e6825f1b46180908c47e2857ae9a58613b
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-07-02 19:12:20 +0530

    done

--- src/components/services/HeroServices.tsx
+++ src/components/services/HeroServices.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroServices: React.FC = () => {
   return (
-    <div className="relative h-[50vh] md:h-[70vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('./imgs/hero2.jpg')" }}>
+    <div className="relative h-[50vh] md:h-[80vh] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('./imgs/hero2.jpg')" }}>
       {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
